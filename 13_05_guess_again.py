b84759b6d74a091d71b965620008b14dd053c643
jhash1 <jhashem3@yahoo.com>
2021-09-03 10:17:55 -0400

    Update 13_05_guess_again.py

--- 13_05_guess_again.py
+++ 13_05_guess_again.py
@@ -4,16 +4,26 @@
 
 
 
-import random
-
-guess = random.randint(1,10)
+number = 6
+number = int(number)
 
 count = 0
 
-value = input("Guess a number between 1 and 10:")
-    while guess != value:
-count += 1
-        print("you win")
+while True:
+    guess = input("Pick one num between 1 and 10: ")
+    if count == 3:
+        print("out of guesses")
+        break
+    guess = int(guess)
+    if number != guess:
+        count += 1
+        print("keep going")
+    elif number == guess:
+        print("you got it")
+        break
+   
+
+
 
 
 # use a number generator to create random num between x and y
